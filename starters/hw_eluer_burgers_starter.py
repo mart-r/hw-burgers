282bd0fabec7f38e518affd029206a869a22a2fc
Mart Ratas <mart.ratas@gmail.com>
2019-10-09 16:15:08 +0300

    Now running for multiple J and nu if needed.

--- starters/hw_eluer_burgers_starter.py
+++ starters/hw_eluer_burgers_starter.py
@@ -9,12 +9,22 @@ if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
 def parse_nu(arg):
+    vals = arg.split(',')
+    nu = []
+    for curVal in vals:
+        cnu = parse_nu_2(curVal)
+        nu.append(cnu)
+    return nu
+
+
+def parse_nu_2(arg):
     try:        
         nu = float(arg)
     except ValueError:
         # try eval
         nu = eval(arg)
     return nu
+
 
 def parse_bool(argIn):
     arg = ''
@@ -27,8 +37,8 @@ def parse_bool(argIn):
 
 
 def parse_arguments(args):
-    _usage1 = 'arguments:  J   nu     tf     bHO    nua'
-    _usage2 = 'arg types: INT FLOAT FLOAT BOOL/INT FLOAT'
+    _usage1 = 'arguments:    J            nu          tf     bHO    nua'
+    _usage2 = 'arg types: INT:INT FLOAT:FLOAT:FLOAT FLOAT BOOL/INT FLOAT'
     _usage = _usage1 + '\n' + _usage2
     # defaults
     tf = 1/2
@@ -41,7 +51,11 @@ def parse_arguments(args):
         print(_usage)
         raise Exception("Need at least 2 arguments! (J and nu)")
     try:
-        J = int(args[0])
+        vals = args[0].split(':')
+        if (len(vals) != 2):
+            raise ValueError
+        J1 = int(vals[0])
+        J2 = int(vals[1])
     except ValueError:
         print(_usage)
         raise Exception("Value for J needs to be an integer!")
@@ -73,6 +87,7 @@ def parse_arguments(args):
             bFindExact = float(args[5])
         except ValueError:
             bFindExact = True 
+    J = range(J1, J2+1)
     return J, nu, tf, bHO, nua, bFindExact
 
 
@@ -81,13 +96,15 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     u01 = 1 # not changing, for now at least
     L = 1   # not changing, for now at least
-    J, nu, tf, bHO, nua, bFindExact = parse_arguments(args)
-    print('J, nu, tf, bHO, nua, bFindExact')
-    print(J, nu, tf, bHO, nua, bFindExact)
-    X, T, U, Ue = solver(J, nu, tf, summax, u01, L, bHO, nua, bFindExact)
-    fname = 'HW_Burgers_AS_J=%d_nu=%f_tf=%d_nua=%f'%(J, nu, tf, nua)
-    if bHO:
-        fname += "_HO"
-    fname += ".mat"
-    plot_results(X, T,U ,Ue, False)
-    saver(fname, X, T, U, Ue)
+    JAll, nuAll, tf, bHO, nua, bFindExact = parse_arguments(args)
+    for J in JAll:
+        for nu in nuAll:
+            print('J, nu, tf, bHO, nua, bFindExact')
+            print(J, nu, tf, bHO, nua, bFindExact)
+            X, T, U, Ue = solver(J, nu, tf, summax, u01, L, bHO, nua, bFindExact)
+            fname = 'HW_Burgers_AS_J=%d_nu=%f_tf=%d_nua=%f'%(J, nu, tf, nua)
+            if bHO:
+                fname += "_HO"
+            fname += ".mat"
+            plot_results(X, T,U ,Ue, False)
+            saver(fname, X, T, U, Ue)
