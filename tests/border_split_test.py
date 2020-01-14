3c224856ab82dd8438044b0358159ecbe111506c
Mart Ratas <mart.ratas@gmail.com>
2020-01-14 16:25:04 +0200

    Now using method within other class

--- tests/border_split_test.py
+++ tests/border_split_test.py
@@ -6,21 +6,13 @@ sys.path.append('/home/mart/Documents/KybI/2019/python/hw-burgers')
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 import numpy as np
+from kdvnumiddle.calc_kdv_dyn import get_smart_grid_left_right
 
 def do_test(N=4, skip=0.2, step=0.05):
     print("N=%d"%N)
     for left in np.arange(0, 1 - skip + step, step):
         right = 1 - left - skip
-        nrl = N * left /(left  + right)
-        nrr = N * right /(left + right)
-        # print ("%5.4f, %5.4f -> %5.4f + %5.4f = %5.4f"%(left, right, nrl, nrr, nrl+nrr), end='')
-        nrl, nrr = int(nrl), int(nrr)
-        # print (": %2d + %2d = %2d"%(nrl, nrr, nrl+nrr))
-        if nrl + nrr < N:
-            if nrl/left < nrr/right:
-                nrl, nrr = nrl + 1, nrr
-            else:
-                nrl, nrr = nrl, nrr + 1
+        nrl, nrr = get_smart_grid_left_right(N, left, right)
         print ("%2d, %2d"%(nrl, nrr), nrl+nrr)
 
 
