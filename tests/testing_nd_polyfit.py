1f915cebb00f906f1e7e19a314274ed1cdc553ca
Mart Ratas <mart.ratas@gmail.com>
2020-01-30 15:46:21 +0200

    Added test for 4D data - working like a charm

--- tests/testing_nd_polyfit.py
+++ tests/testing_nd_polyfit.py
@@ -43,3 +43,18 @@ if __name__ == "__main__":
     diff = p2(XX, YY, ZZ) - VAL
     # print("And diff:", diff)
     print('MAX difF:', np.max(np.abs(diff)))
+
+    print("Testing 4D")
+    z2 = np.linspace(0, 1, 5)
+    XX, YY, ZZ, Z2Z2 = np.meshgrid(x, y, z, z2)
+    print('shapes of input:', XX.shape, YY.shape, ZZ.shape, Z2Z2.shape)
+
+    VAL = XX**2 - YY**2 + 2 * ZZ + 6 * Z2Z2
+    print('shape of data:', VAL.shape)
+
+    p2 = NDPolyFitter((XX, YY, ZZ, Z2Z2), VAL, tol=1e-10)
+    print("Got fitter:", p2)
+    print("With coefficients:", p2.coefs)
+    diff = p2(XX, YY, ZZ, Z2Z2) - VAL
+    # print("And diff:", diff)
+    print('MAX difF:', np.max(np.abs(diff)))
